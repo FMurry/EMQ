231dc81df62d70ca1a32af5c0fc51fe99765baed
FMurry <frederic.wesley.murry@gmail.com>
2016-09-30 16:34:28 -0700

    Added Cart.php and migration

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateCartTable' => $baseDir . '/database/migrations/2016_09_30_232904_CreateCartTable.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
     'CreateProductsTable' => $baseDir . '/database/migrations/2016_09_20_034605_create_products_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
