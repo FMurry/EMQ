c83d01f36d81906d678b825cd69b9febdc6f408c
emmendoza1986 <emmendoza1986@gmail.com>
2016-09-30 21:23:32 -0700

    Added Cart add function and remove function.

--- app/Cart.php
+++ app/Cart.php
@@ -15,24 +15,6 @@ class Cart extends Model
     protected $table = 'cart';
 
 
-    public function exists($userId, $productId){
-    	$query = DB::table('cart')->where('user_id', $userId)->where('product_id', $productId)->exist();
-    	if($query){
-    		DB::table('cart')->where('user_id', $userId)->where('product_id',$productId)->increment('quantity');
-    	}
-    	else{
-    		DB::table('cart')->insert([
-    			['user_id' => $userId],
-    			['product_id' => $productId],
-    			['quantity' => 1]
-    		]);
-    	}
-    }
-
-    public function incrementCartProductQuantity(){
-        $this->increment('quantity');
-    }
-
 
 
 }
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,38 +14,75 @@ use Illuminate\Support\Facades\DB;//Needed to use DB::
 
 class CartController extends Controller
 {
+    /*
+    *   Checks if cart entry with product exists in users cart.
+    *   If it exists, increment Cart Quantity
+    *   Products->isAvailable() returns bool and Decrements Product inventory quantity by 1.
+    *   Else make a new cart entry.
+    */
     public function addToCart($product_id)
     {
-    	//if exists
-    	$product = Products::find($product_id);
-    	$user_id = Auth::user()->id;
-    	
-    	if($product){
-    		if( $product->ifAvailable() ){
-    			echo "item exists, item decremented<br>";//testing
-    			$cartExist = DB::table('cart')->where('user_id', $user_id )->where('products_id', $product_id )->first();
+        
+        $product = Products::find($product_id);
+        $user_id = Auth::user()->id;
 
-    			echo "cart id: "+$cartExist->id+"<br>";
-    			
-    			if($cartExist){
-    				$cart = Cart::find( $cartExist['id'] );
-    				$cart->$incrementCartProductQuantity();
-    				echo "cart quantity incremented to:"+ $cartExist->quantity + "<br>";//testing
-    			}else{
-    				echo "cart entry doesn't exist. Creating entry.<br>";//testing
-    				DB::table('cart')->insert([
-		    			'user_id' => $user_id,
-		    			'products_id' => $product_id,
-		    			'quantity' => 1,
-		    		]);
-    			}
-    			echo " END of adding to cart.";
-    			return $product->quantity;
-    		}
-    		return "item exists, item not available";
-    	}else{
-    		return "product doesn't exist";
-    	}
-    	//return view('product', ['product' => $product]);
+        // IF product exists
+        if($product){
+
+            // IF product is Available, quantity > 0
+            if( $product->isAvailable() ){
+
+                // Retrieve User Cart Entry with Product
+                $cartExist = DB::table('cart')->where('user_id', $user_id )->where('product_id', $product_id )->first();
+
+                if($cartExist){
+                    // Cart entry with product exists, incrementing quantity
+                    $cart = Cart::find( $cartExist->id )->increment('quantity');
+                }else{
+                    // cart entry doesn't exist. Creating entry
+                    $cart = new Cart;
+                    $cart->user_id = $user_id;
+                    $cart->product_id = $product_id;
+                    $cart->quantity = 1;
+                    $cart->save();//ALWAYS SAVE CHANGES
+                }
+                return "Successfully Added to Cart.";
+            }
+            return "Sorry, This Item is Temporarily Unavailable.";
+        }
+        return "Error, This Product Doesn't Exist.";
+    }
+
+    /*
+    *   Checks if cart entry with product exists in users cart.
+    *   If it exists, decrement cart quantity, increment product inventory quantity.
+    *   If Cart quantity is zero, delete table row
+    */
+    public function removeFromCart($product_id){
+
+        $user_id = Auth::user()->id;
+        $cartExist = DB::table('cart')->where('user_id', $user_id )->where('product_id', $product_id )->first();
+
+        if($cartExist){
+            $cart = Cart::find( $cartExist->id );
+
+            //If Quantity > 1, decrement
+            //Else Delete Cart Table Row
+            if( $cart->quantity > 1){
+                $cart->decrement('quantity');
+            }else{
+                $cart->delete();
+            }
+
+            //Code to store Products quantity +1
+
+            $product = Products::find($product_id);
+            echo "Before: ".$product->quantity;
+            $product->increment('quantity');
+            echo "After: ".$product->quantity;
+            $product->save();
+            return "Item Successfully Removed from Cart.";
+        }
+        return "Item Not In Cart.";
     }
 }
