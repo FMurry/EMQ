86411a90b9fe985cd9996b356a55cf8a5b9c4d5b
FMurry <frederic.wesley.murry@gmail.com>
2016-09-30 17:04:50 -0700

    Updated Cart model

--- app/Cart.php
+++ app/Cart.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Cart extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'cart';
+
+
+    public function exists($userId, $productId){
+    	$query = DB::table('cart')->where('user_id', $userId)->where('product_id', $productId)->exist();
+    	if($query){
+    		DB::table('cart')->where('user_id', $userId)->where('product_id',$productId)->increment('quantity')
+    	}
+    	else{
+    		DB::table('cart')->insert([
+    			['user_id' => $userId],
+    			['product_id' => $productId],
+    			['quantity' => 1]
+    		]);
+    	}
+    }
+
+
 }
