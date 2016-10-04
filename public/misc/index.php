838f1a8aaccde1712cbb31571378e708bef81499
emmendoza1986 <emmendoza1986@gmail.com>
2016-10-04 15:12:13 -0700

    Added test files for dynamic content.

---
+++ public/misc/index.php
@@ -0,0 +1,24 @@
+    <!doctype html>
+    <html>
+     <head>
+        <meta charset=utf-8>
+        <title>Test1</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="functions.js"></script>
+      </head>
+     <body>
+       <center>
+	 <h1>Account Status</h1>
+	 <div id="net"></div>
+   <div id="total"></div>     
+   <div id="btcFree"></div>     
+   <div id="cnyFree"></div>
+   <div id="btcFreezed"></div>     
+   <div id="cnyFreezed"></div>
+
+   <h1>Order History</h1> 
+   <div id="orders"></div>          
+       </center>
+     </body>
+    </html>
+
