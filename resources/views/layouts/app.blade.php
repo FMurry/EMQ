35d6e81cba3d12692c145be8df02d801062dc0a0
emmendoza1986 <emmendoza1986@gmail.com>
2016-10-06 15:25:16 -0700

    Added account management view with buttons to modify other account information.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                 <li><a href="{{ url('/edit') }}"><i class="fa fa-btn fa-edit"></i>Account Management</a></li>
+                                 <li><a href="{{ url('/account') }}"><i class="fa fa-btn fa-edit"></i>Account Management</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                             <li><a href="{{ url('/cart') }}"><i class="fa fa-btn
