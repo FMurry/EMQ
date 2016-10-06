b9d028efe81621ddeb19ee4fdb0d97e6fab7ab28
emmendoza1986 <emmendoza1986@gmail.com>
2016-10-05 19:00:43 -0700

    modified add payment view and modified route.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Payment; //Import Payment to Controller
+use Illuminate\Support\Facades\Auth;//Needed to use Auth::
+use Illuminate\Support\Facades\DB;//Needed to use DB::
+
 class PaymentController extends Controller
 {
-    //
+    /*
+    * Testing to see if Form can Update Database
+    */
+    public function addPaymentMethod(Request $request){
+
+    	return $request;
+    	/*
+    	Auth::user()->name = $newFullName; //Update cached variable
+
+
+    	DB::table('users')
+    		->where('id', Auth::user()->id)
+    		->update(['name' => $newFullName]); //Update database entry
+	
+    	return view('/home');*/
+
+    }
 }
