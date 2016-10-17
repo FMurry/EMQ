96b57cdae83ff7acadf0abc78624c3951e6db6d4
nkha149 <ha.nguyen@sjsu.edu>
2016-10-17 14:17:23 -0700

    Add breadcrumb, dropdown menu for State input field, and locked country input field as United States

--- resources/views/account/add_address.blade.php
+++ resources/views/account/add_address.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<ol class="breadcrumb">
+  <li><a href="http://localhost/emq/public/account">Account Management</a></li>
+  <li><a href="http://localhost/emq/public/account/address">Address Book</a></li>
+  <li class="active">Add New Address</li>
+</ol>
+
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
@@ -25,29 +31,91 @@
  	{!! csrf_field() !!}
   <div class="form-group">
     <label for="newFullName">Full Name:</label>
-    <input type="text" class="form-control" id="newFullName" name="newFullName">
+    <input type="text" class="form-control" id="newFullName" name="newFullName" placeholder="Enter Full Name">
+
     <label for="newAddress">Address</label>
-    <input type="text" class="form-control" id="newAddress" name="newAddress">
+    <input type="text" class="form-control" id="newAddress" name="newAddress" placeholder="Enter Number and Street">
+
     <label for="newAddress2">Address Line 2</label>
-    <input type="text" class="form-control" id="newAddress2" name="newAddress2">
-    <label for="newCity">City</label>
-    <input type="text" class="form-control" id="newCity" name="newCity">
-    <label for="newState">State</label>
-    <input type="text" class="form-control" id="newState" name="newState">
-    <label for="newZip">Zip</label>
-    <input type="text" class="form-control" id="newZip" name="newZip">
+    <input type="text" class="form-control" id="newAddress2" name="newAddress2" placeholder="Apt., Unit #, etc.">
+
+    <div class="row">
+        <div class="col-xs-3">
+            <label for="newCity">City</label>
+            <input type="text" class="form-control" id="newCity" name="newCity" placeholder="Enter City">
+        </div>
+        <div class="col-xs-3">
+            <label for="newState">State</label>
+            <select class="form-control" id="newState" name="newState">
+                <option>Alabama</option>
+                <option>Alaska</option>
+                <option>Arizona</option>
+                <option>Arkansas</option>
+                <option>California</option>
+                <option>Colorado</option>
+                <option>Connecticut</option>
+                <option>Delaware</option>
+                <option>Florida</option>
+                <option>Georgia</option>
+                <option>Hawaii</option>
+                <option>Idaho</option>
+                <option>Illinois</option>
+                <option>Indiana</option>
+                <option>Iowa</option>
+                <option>Kansas</option>
+                <option>Kentucky</option>
+                <option>Louisiana</option>
+                <option>Maine</option>
+                <option>Maryland</option>
+                <option>Massachusetts</option>
+                <option>Michigan</option>
+                <option>Minnesota</option>
+                <option>Mississippi</option>
+                <option>Missouri</option>
+                <option>Montana</option>
+                <option>Nebraska</option>
+                <option>Nevada</option>
+                <option>New Hampshire</option>
+                <option>New Jersey</option>
+                <option>New Mexico</option>
+                <option>New York</option>
+                <option>North Carolina</option>
+                <option>North Dakota</option>
+                <option>Ohio</option>
+                <option>Oklahoma</option>
+                <option>Oregon</option>
+                <option>Pennsylvania</option>
+                <option>Rhode Island</option>
+                <option>South Carolina</option>
+                <option>South Dakota</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Utah</option>
+                <option>Vermont</option>
+                <option>Virginia</option>
+                <option>Washington</option>
+                <option>West Virginia</option>
+                <option>Wisconsin</option>
+                <option>Wyoming</option>
+            </select>
+        </div>
+        <div class="col-xs-3">
+            <label for="newZip">Zip</label>
+            <input type="text" class="form-control" id="newZip" name="newZip" placeholder="Enter Zipcode">
+        </div>
+    </div>
+    
     <label for="newCountry">Country</label>
-    <input type="text" class="form-control" id="newCountry" name="newCountry">
+    <input type="text" class="form-control" id="newCountry" name="newCountry" value ="United States" readonly="readonly">
+
     <label for="newPhone">Phone</label>
-    <input type="text" class="form-control" id="newPhone" name="newPhone">
+    <input type="text" class="form-control" id="newPhone" name="newPhone" placeholder="Enter Phone Number">
   </div>
   
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 <!-- Stuff I added -->
-
-
                 </div>
             </div>
         </div>
