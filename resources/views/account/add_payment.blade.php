8f3626922b8b696466ad7279ff80b45a49351a72
nkha149 <ha.nguyen@sjsu.edu>
2016-10-17 14:18:23 -0700

    Add breadcrumb, and dropdown menu for Month input field

--- resources/views/account/add_payment.blade.php
+++ resources/views/account/add_payment.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<ol class="breadcrumb">
+  <li><a href="http://localhost/emq/public/account">Account Management</a></li>
+  <li><a href="http://localhost/emq/public/account/payment">Payment Methods</a></li>
+  <li class="active">Add New Payment Method</li>
+</ol>
+
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
@@ -31,14 +37,33 @@
     <label for="newFullName">Card Number:</label>
     <input type="text" class="form-control" id="newFullName" name="cardNumber">
 
-    <label for="newFullName">Expiration Month:</label>
-    <input type="text" class="form-control" id="newFullName" name="expirationMonth">
+    <div class="row">
+        <div class="col-xs-2">
+            <label for="newFullName">Expiration Month:</label>
+            <select class="form-control" id="expirationMonth" name="expirationMonth">
+                <option>01</option>
+                <option>02</option>
+                <option>03</option>
+                <option>04</option>
+                <option>05</option>
+                <option>06</option>
+                <option>07</option>
+                <option>08</option>
+                <option>09</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
+            </select>
+        </div>
+        <div class="col-xs-2">
+            <label for="newFullName">Expiration Year:</label>
+            <input type="text" class="form-control" id="expirationYear" name="expirationYear">
+        </div>
+</div>
 
-    <label for="newFullName">Expiration Year:</label>
-    <input type="text" class="form-control" id="newFullName" name="expirationYear">
-  </div>
-  
-  <button type="submit" class="btn btn-default">Submit</button>
+<div class="">
+    <button type="submit" class="btn btn-primary">Submit</button>
+</div>
 </form>
 
 <!-- Stuff I added -->
