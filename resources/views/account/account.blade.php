bfe1ca7356548e6f427821fd4d451f6c7a096d66
nkha149 <ha.nguyen@sjsu.edu>
2016-10-17 14:15:37 -0700

    Add icon for each functionality, add breadcrumb

--- resources/views/account/account.blade.php
+++ resources/views/account/account.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<ol class="breadcrumb">
+  <li class="active">Account Management</li>
+</ol>
+
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-16 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Account Management</div>
 
@@ -11,11 +15,44 @@
                     
 
 <!-- Stuff I added -->
-<a href="{{ url('/edit') }}" class="btn btn-default">Manage Account Details</a><br>
-<a href="{{ action('CartController@getCart') }}" class="btn btn-default">Manage Shopping Cart</a><br>
-<a href="{{ action('AddressController@getAddress') }}" class="btn btn-default">Manage Shipping Addresses</a><br>
-<a href="{{ action('PaymentController@getPaymentMethods') }}" class="btn btn-default">Manage Payment Methods</a><br>
-
+                    <div class="row" style="width: 100%; text-align: center;">
+                        <div class="col-md-3">
+                            <div class="panel panel-warning" style="margin: 10px;">
+                                <div href="{{ url('/edit') }}" class="panel-heading">Account Details</div>
+                                <div class="panel-body">
+                                    <img src="http://image.flaticon.com/icons/svg/138/138669.svg" alt="Profile" style="width:180px; height:180px; margin: 5px;">
+                                    <a href="{{ url('/edit') }}" class="btn btn-default">Manage Account Details</a><br>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div class="col-md-3">
+                            <div class="panel panel-warning" style="margin: 10px;">
+                                <div class="panel-heading">Shopping Cart</div>
+                                <div class="panel-body">
+                                    <img src="http://image.flaticon.com/icons/svg/204/204283.svg" alt="Cart" style="width:180px; height:180px; margin: 5px;">
+                                    <a href="{{ action('CartController@getCart') }}" class="btn btn-default">Manage Shopping Cart</a><br>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div class="col-md-3">
+                            <div class="panel panel-warning" style="margin: 10px;">
+                                <div class="panel-heading">Address Book</div>
+                                <div class="panel-body">
+                                    <img src="http://image.flaticon.com/icons/svg/232/232508.svg" alt="AddressBook" style="width:180px; height:180px; margin: 5px;">
+                                    <a href="{{ action('AddressController@getAddress') }}" class="btn btn-default">Manage Addresses</a><br>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div class="col-md-3">
+                            <div class="panel panel-warning" style="margin: 10px;">
+                                <div class="panel-heading">Payment Methods</div>
+                                <div class="panel-body">
+                                    <img src="http://image.flaticon.com/icons/svg/235/235805.svg" alt="Payment" style="width:180px; height:180px; margin: 5px;">
+                                    <a href="{{ action('PaymentController@getPaymentMethods') }}" class="btn btn-default">Manage Payment Methods</a><br>                              
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
 
 <!-- Stuff I added -->
 
