61bb51fcb1d5a50e091272b9957beb4b0c4b9c5e
David Navarro <David Navarro>
2016-10-20 15:06:42 -0700

    Replace angular table with panels of all products. Need to dynamically replace image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,9 @@
   app.controller('customersCtrl', function($scope, $http) {
       $http.get("http://localhost/emq/public/api?data=products")
       .then(function (response) {$scope.names = response.data.products;});
+      $scope.random = function() {
+        return 0.5 - Math.random();
+      }
   });
   </script>
     <!-- End of Scripts Added to Body Section -->
@@ -33,33 +36,39 @@
                     <b>Inventory</b>
 
 <!-- Stuff I added -->
-<style>
-table, th , td  {
-  border: 1px solid grey;
-  border-collapse: collapse;
-  padding: 5px;
-}
-table tr:nth-child(odd) {
-  background-color: #f1f1f1;
-}
-table tr:nth-child(even) {
-  background-color: #ffffff;
-}
-</style>
+                  <style>
+                  table, th , td  {
+                    border: 1px solid grey;
+                    border-collapse: collapse;
+                    padding: 5px;
+                  }
+                  table tr:nth-child(odd) {
+                    background-color: #f1f1f1;
+                  }
+                  table tr:nth-child(even) {
+                    background-color: #ffffff;
+                  }
+                  </style>
 
-<div ng-app="myApp" ng-controller="customersCtrl"> 
+                  <div ng-app="myApp" ng-controller="customersCtrl"> 
 
-<table>
-    <tr><!--<th>#</th>--><th>Qty</th><th>Product</th></tr>
-  <tr ng-repeat="x in names">
-    <!--<td>@{{ $index + 1 }}</td>-->
-    <td>@{{ x.quantity }}</td>
-    <td><a href="./product/@{{ x.id }}" >@{{ x.productName }}</a></td>
-    <!--<td>@{{ x.productName }}</td>-->
-  </tr>
-</table>
+                    <div class="row" style="width: 100%; text-align: center;">
+                      <div ng-repeat="x in names|orderBy: random">
+                        <div class="col-md-4">
+                            <div class="panel panel-warning" style="margin: 10px;">
+                                <div class="panel-heading" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                                @{{ x.productName }}
+                                </div>
+                                <div class="panel-body">
+                                    <img src="http://image.flaticon.com/icons/svg/214/214273.svg" alt="Box" style="width:90%; height:90%; margin: 5px;">
+                                    <button class="btn btn-default">Add to Cart</button>
+                                </div>
+                            </div>                            
+                        </div>
+                      </div>                      
+                    </div>
 
-</div>
+                  </div>
 
 
 
