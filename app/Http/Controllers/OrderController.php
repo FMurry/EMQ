42c379c2211d824c72b1faca811c87b528fa18c1
FMurry <frederic.wesley.murry@gmail.com>
2016-10-23 19:27:52 -0700

    Added OrderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Order; //Import Payment to Controller
+use App\OrderPayment; 
+use App\OrderAddress; //Import Payment to Controller
+use App\Store; //Import Payment to Controller
+use App\OrderProducts; //Import Payment to Controller
+
+use Illuminate\Support\Facades\Auth;//Needed to use Auth::
+use Illuminate\Support\Facades\DB;//Needed to use DB::
+
+class OrderController extends Controller
+{
+    
+}
