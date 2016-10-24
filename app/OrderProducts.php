ea3c6335737ee2106f1c1223f9ca1520530fe35c
FMurry <frederic.wesley.murry@gmail.com>
2016-10-23 19:19:01 -0700

    Updated DB relations

--- app/OrderProducts.php
+++ app/OrderProducts.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderProducts extends Model
 {
-    //
+    /*
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'orders';
+
+
+    public function product(){
+    	return $this->hasOne('App/Products','id','product_id');
+    }
 }
