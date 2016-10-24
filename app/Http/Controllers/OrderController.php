cb8b3ec01dc736ba9b50a0562ff16f6b6160adb3
emmendoza1986 <emmendoza1986@gmail.com>
2016-10-23 19:53:06 -0700

    moved completeorder function to order controller.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use App\Cart;
 use App\Order; //Import Payment to Controller
 use App\OrderPayment; 
 use App\OrderAddress; //Import Payment to Controller
@@ -17,5 +17,15 @@ use Illuminate\Support\Facades\DB;//Needed to use DB::
 
 class OrderController extends Controller
 {
-    
+    public function completeOrder(Request $request){
+        $this->validate($request, [
+            'address' => 'required|integer|exists:address,id',//Can be hacked, need to validate address belongs to user
+            'payment' => 'required|integer|exists:payment,id',//Can be hacked, need to validate payment method belongs to user
+            'total' => 'required',
+        ]);
+
+
+        $data = json_encode($request->all());
+        return view('process.complete', ['OrderData' => $data]);
+    }
 }
