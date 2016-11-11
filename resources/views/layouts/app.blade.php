ead6c4a3229fb13af4a9282228f33fabf207ae70
emmendoza1986 <emmendoza1986@gmail.com>
2016-11-11 00:17:03 -0800

    If Admin unlists an item, customers will not be able to add item to their cart now. ALso, Cart icon on top right now displays cart quantity as well.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/shop') }}"><i class="fa fa-btn fa-th"></i>Shop EMQ</a></li>
                                 <li><a href="{{ url('/account') }}"><i class="fa fa-btn fa-edit"></i>Account Management</a></li>
                                 <li><a href="{{ url('/account/orders') }}"><i class="fa fa-btn fa-reorder"></i>Order History</a></li>
                                 @if(Auth::user()->access() >= 1)
@@ -80,7 +81,7 @@
                                 @endif
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
-                            <li><a href="{{ url('/cart') }}"><i class="fa fa-btn
+                            <li><a href="{{ url('/cart') }}"><sub>{{ Auth::user()->cart() }}</sub><i class="fa fa-btn
                                  fa-shopping-cart"></i>Cart</a></li>
                         </li>
                     @endif
