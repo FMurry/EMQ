0bbfcb0162a09b8af062317a6eefcde6753a0ea5
FMurry <frederic.wesley.murry@gmail.com>
2017-02-02 19:55:36 -0800

    git fix

---
+++ resources/views/about.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">About us</div>
+
+                <div class="panel-body">
+                  <div class="row">
+                    <div class="col-md-4">
+                     Frederic Murry
+                    </div>
+                    <div class="col-md-4">
+                     Emmanuel Mendoza
+                    </div>
+                    <div class="col-md-4">
+                     David Navarro
+                    </div>
+                    <div class="col-md-4">
+                     Michael Ngyen
+                    </div>
+                    <div class="col-md-4">
+                     Ha Ngyen
+                    </div>
+                  </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
